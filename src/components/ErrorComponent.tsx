0bed6b9bcd4d4a2d4349a41d4dfb9e4b311f171f
hieulnt-aiot <hieulnt@aiot-global.com>
2025-06-23 08:49:32 +0700

    feat: add test components

---
+++ src/components/ErrorComponent.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import React from 'react';
+
+export default function ErrorComponent() {
+  const msg = 'Hello World';
+  console.log(msg);
+  return <div> {msg}</div>;
+}
---
+++ src/components/ValidComponent.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+
+export default function ValidComponent() {
+  const message = 'Hello World';
+  return <div>{message}</div>;
+}
