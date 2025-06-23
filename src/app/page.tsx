74117e14840ef720f98715b7aec52ee28e2b7953
hieulnt-aiot <hieulnt@aiot-global.com>
2025-06-23 09:09:03 +0700

    feat: change page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import ValidComponent from '@/components/ValidComponent';
+// import ValidComponent from '@/components/ValidComponent';
 // import ErrorComponent from '@/components/ErrorComponent';
 
 export default function HomePage() {
   return (
     <main className="p-4">
-      <h1 className="text-xl font-bold mb-4">CI/CD Test Page</h1>
-      <ValidComponent />
-      {/* <ErrorComponent /> */}
+      <h1>Test Deploy change</h1>
     </main>
   );
 }
